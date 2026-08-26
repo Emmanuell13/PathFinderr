c698c0e9c1c4ee1bbe254b74317a5ea8f184740e
haidar2505 <haidar.ameermeea@gmail.com>
2026-08-26 14:53:01 +0400

    .env correction

--- PathFinder/src/main/java/module-info.java
+++ PathFinder/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.mycompany.pathfinder {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires io.github.cdimascio.dotenv.java;
 
     opens com.mycompany.pathfinder to javafx.fxml;
     exports com.mycompany.pathfinder;
